5d2f9a45afce7c3bc7a78a1c7b3926260614ff9c
Admin <bayartsengelt@gmail.com>
2021-05-24 16:51:01 +1000

    update the Grading

--- edit.php
+++ edit.php
@@ -14,12 +14,26 @@ require_once(dirname(__FILE__) . '/../../config.php');
 $PAGE->set_url(new moodle_url('/local/autograder/edit.php'));
 $PAGE->set_context(\context_system::instance());
 $PAGE->set_title('Edit Autograder');
-
+global $CFG, $DB;
 
 echo $OUTPUT->header();
 
 echo "Test Page";
 
-grade_get_grades();
+$courseid = 383;
+
+$gradebookgrades = grade_get_grades($courseid, 'mod', 'quiz',898, 99);
+
+$data = new stdClass();
+$data->finalgrade = 95;
+$data->userid = 99;
+$data->id = 194762;
+
+$DB->update_record('grade_grades',$data);
+
+$gradebookgrades2 = grade_get_grades(381, 'mod', 'quiz',1112, 99);
+
+print_object($gradebookgrades);
+print_object($gradebookgrades2);
 
 echo $OUTPUT->footer();
